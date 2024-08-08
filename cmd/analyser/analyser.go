07576c85a06a7d5651df8c222f75b00ab716554c
Bastien Beuchat <bastien.beuchat@epfl.ch>
2024-08-08 17:21:52 +0200

    Add competition overall stats

--- cmd/analyser/analyser.go
+++ cmd/analyser/analyser.go
@@ -25,28 +25,11 @@ func main() {
 		return
 	}
 
+	overallWinRecords := []analyser.WinRecord{}
 	for _, category := range competition.Categories {
 		winRecords := analyser.ParseWinRecords(category.Contests)
-		winByTypes := analyser.GroupByWinType(winRecords)
-		winByGolden := analyser.GroupByGoldenScore(winRecords)
-
-		fmt.Println("====================================")
-		fmt.Printf("Competition: %s\n", competition.Name)
-		fmt.Printf("Category: %s\n", category.Name)
-		fmt.Printf("# fights: %d\n", len(winRecords))
-		fmt.Println("====================================")
-		fmt.Printf("# wins by ippon: %d %s\n", winByTypes[analyser.WinByIppon], formatPercentage(winByTypes[analyser.WinByIppon], len(winRecords)))
-		fmt.Printf("# wins by waza-ari: %d %s\n", winByTypes[analyser.WinByWaza], formatPercentage(winByTypes[analyser.WinByWaza], len(winRecords)))
-		fmt.Printf("# wins by 3 shidos: %d %s\n", winByTypes[analyser.WinByShido], formatPercentage(winByTypes[analyser.WinByShido], len(winRecords)))
-		fmt.Printf("# wins by direct hansoku-make: %d %s\n", winByTypes[analyser.WinByHansokuMake], formatPercentage(winByTypes[analyser.WinByHansokuMake], len(winRecords)))
-		fmt.Println("====================================")
-		fmt.Printf("# wins in regular time: %d %s\n", winByGolden[false], formatPercentage(winByGolden[false], len(winRecords)))
-		fmt.Printf("# wins in Golden Score: %d %s\n", winByGolden[true], formatPercentage(winByGolden[true], len(winRecords)))
-		fmt.Println("====================================")
+		analyser.DisplayWinRecords(competition.Name, category.Name, winRecords)
+		overallWinRecords = append(overallWinRecords, winRecords...)
 	}
-}
-
-func formatPercentage(part int, total int) string {
-	percentage := float64(part) / float64(total) * 100
-	return fmt.Sprintf("(%.2f%%)", percentage)
+	analyser.DisplayWinRecords(competition.Name, "all", overallWinRecords)
 }
--- pkg/analyser/report.go
+++ pkg/analyser/report.go
@@ -1,6 +1,27 @@
 package analyser
 
-func GroupByWinType(winRecords []WinRecord) map[WinType]int {
+import "fmt"
+
+func DisplayWinRecords(competitionName string, categoryName string, winRecords []WinRecord) {
+	winByTypes := groupByWinType(winRecords)
+	winByGolden := groupByGoldenScore(winRecords)
+
+	fmt.Println("====================================")
+	fmt.Printf("Competition: %s\n", competitionName)
+	fmt.Printf("Category: %s\n", categoryName)
+	fmt.Printf("# fights: %d\n", len(winRecords))
+	fmt.Println("====================================")
+	fmt.Printf("# wins by ippon: %d %s\n", winByTypes[WinByIppon], formatPercentage(winByTypes[WinByIppon], len(winRecords)))
+	fmt.Printf("# wins by waza-ari: %d %s\n", winByTypes[WinByWaza], formatPercentage(winByTypes[WinByWaza], len(winRecords)))
+	fmt.Printf("# wins by 3 shidos: %d %s\n", winByTypes[WinByShido], formatPercentage(winByTypes[WinByShido], len(winRecords)))
+	fmt.Printf("# wins by direct hansoku-make: %d %s\n", winByTypes[WinByHansokuMake], formatPercentage(winByTypes[WinByHansokuMake], len(winRecords)))
+	fmt.Println("====================================")
+	fmt.Printf("# wins in regular time: %d %s\n", winByGolden[false], formatPercentage(winByGolden[false], len(winRecords)))
+	fmt.Printf("# wins in Golden Score: %d %s\n", winByGolden[true], formatPercentage(winByGolden[true], len(winRecords)))
+	fmt.Println("====================================")
+}
+
+func groupByWinType(winRecords []WinRecord) map[WinType]int {
 	winByTypes := map[WinType]int{
 		WinByIppon:       0,
 		WinByWaza:        0,
@@ -14,7 +35,7 @@ func GroupByWinType(winRecords []WinRecord) map[WinType]int {
 	return winByTypes
 }
 
-func GroupByGoldenScore(winRecords []WinRecord) map[bool]int {
+func groupByGoldenScore(winRecords []WinRecord) map[bool]int {
 	winByGolden := map[bool]int{
 		true:  0,
 		false: 0,
@@ -24,4 +45,9 @@ func GroupByGoldenScore(winRecords []WinRecord) map[bool]int {
 	}
 
 	return winByGolden
+}
+
+func formatPercentage(part int, total int) string {
+	percentage := float64(part) / float64(total) * 100
+	return fmt.Sprintf("(%.2f%%)", percentage)
 }
